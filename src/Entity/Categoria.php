2b764e6cd2440499a94284d1c166094591d348a9
Luis Ramon Lopez Lopez <lrlopez@gmail.com>
2021-11-30 10:46:33 +0100

    Creada clase Categoria

---
+++ src/Entity/Categoria.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Entity;
+
+class Categoria
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $nombre;
+
+    /**
+     * @var bool
+     */
+    private $visible;
+}
