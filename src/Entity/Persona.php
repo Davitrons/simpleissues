4fed80532ee850911cfaae66e9c8663e7ff185a0
Luis Ramon Lopez Lopez <lrlopez@gmail.com>
2021-11-30 10:31:27 +0100

    Creada entidad Persona

---
+++ src/Entity/Persona.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Entity;
+
+class Persona
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     */
+    private $apellidos;
+}
