b3092bba9d7250d76d41c2ea927e3e595159c0c6
Luis Ramon Lopez Lopez <lrlopez@gmail.com>
2021-11-30 10:55:26 +0100

    Creadas asociación entre Categoria y Persona (gestor)

--- src/Entity/Categoria.php
+++ src/Entity/Categoria.php
@@ -2,22 +2,39 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="categoria")
+ */
 class Categoria
 {
     /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
      * @var int
      */
     private $id;
 
     /**
+     * @ORM\Column(type="string")
      * @var string
      */
     private $nombre;
 
     /**
+     * @ORM\Column(type="boolean")
      * @var bool
      */
     private $visible;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Persona", inversedBy="responsabilidades")
+     * @var Persona
+     */
+    private $gestor;
 
     /**
      * @return int
--- src/Entity/Persona.php
+++ src/Entity/Persona.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -28,6 +30,17 @@ class Persona
      * @var string
      */
     private $apellidos;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Categoria", mappedBy="gestor")
+     * @var Categoria[]|Collection
+     */
+    private $responsabilidades;
+
+    public function __construct()
+    {
+        $this->responsabilidades = new ArrayCollection();
+    }
 
     /**
      * @return int
